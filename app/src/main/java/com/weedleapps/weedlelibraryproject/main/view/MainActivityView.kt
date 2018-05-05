adb4bf9e22d0d446dcf70306a4ba3cb4ba02449b
osapps <osfunapps@gmail.com>
2018-05-05 15:39:37 +0300

    removed app folder

--- app/src/main/java/com/weedleapps/weedlelibraryproject/main/view/MainActivityView.kt
+++
@@ -1,9 +0,0 @@
-package com.weedleapps.weedlelibraryproject.main.view
-
-import android.support.v4.app.FragmentManager
-import com.weedleapps.weedlelibraryproject.di.MainView
-
-/** [MainActivity] **/
-interface MainActivityView : MainView {
-    fun presenterDone()
-}
